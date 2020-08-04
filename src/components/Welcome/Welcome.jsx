b7d3f64fc1b8f0b78b629557800bf5b00ed560aa
inezav <ineza.v@rambler.ru>
2020-08-04 23:26:28 +0300

    Изменение текстового содержимого

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,21 +1,29 @@
-import React from "react";
-import Auth from "./Form/Auth";
+import React from 'react'
+import Auth from './Form/Auth'
+import './welcome.css'
+import "fontsource-roboto"
+import {Typography} from '@material-ui/core';
+
 
 const Title = () => {
-    return (
-        <div className="title">
-
-        </div>
-    )
+	return (
+		<div className="title">
+			<Typography variant="h2">
+				Выбрать компанию себе и любимому питомцу для прогулок!
+			</Typography>
+		</div>
+	)
 }
 
 const Welcome = () => {
-    return (
-        <div className="main">
-            <Title />
-            <Auth />
-        </div>
-    )
+	return (
+		<div className="main">
+			<Title />
+			<Auth />
+		</div>
+	)
 }
+
+export default Welcome
 
 export default Welcome
