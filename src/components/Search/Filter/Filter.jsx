4e160dfcab65b2f56a757adab565dfce467f7f36
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-08-09 00:26:57 +0300

    Added animations for parameters boxes

--- src/components/Search/Filter/Filter.jsx
+++ src/components/Search/Filter/Filter.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
 import './filter.css';
+import './filter.transitions.css';
 import Parameter from './components/parameter/parameter';
+import ParametersBlock from './components/parametersBlock/parametersBlock';
 
 function Filter({getState, onChangeCheckboxes}) {
+	const [openFirst, setOpenFirst] = useState(false);
+	const [openSecond, setOpenSecond] = useState(false);
+
+	const [menuHeightFirst, setMenuHeightFirst] = useState(21);
+	const [menuHeightSecond, setMenuHeightSecond] = useState(21);
+
+	function calcHeightFirst(el) {
+		if (openFirst) {
+			const height = el.offsetHeight + 26;
+        	setMenuHeightFirst(height);	
+		} else {
+			setMenuHeightFirst(21)
+		}
+	}
+	
+	function calcHeightSecond(el) {
+		if (openFirst) {
+			const height = el.offsetHeight + 26;
+        	setMenuHeightSecond(height);	
+		} else {
+			setMenuHeightSecond(21)
+		}
+    }
+
 	return (
 		<div className='filter'>
 			<div className='filter__title'>
@@ -13,58 +41,42 @@ function Filter({getState, onChangeCheckboxes}) {
 			</div>
 			<div className='filter__items'>
 				<form onChange={(e) => onChangeCheckboxes(e)}>
-					<details className='filter__item'>
-						<summary className='item__subtitle'>
-							Пол
-						</summary>
-						<div className='item__body' >
-							<Parameter name='gender female'>Девочка</Parameter>
-							<Parameter name='gender male'>Мальчик</Parameter>
-						</div>
-					</details>
-					<details className='filter__item'>
-						<summary className='item__subtitle'>
-							Возраст
-						</summary>
-						<div className='item__body'>
-							<Parameter name='age 1'>Меньше 1 года</Parameter>
-							<Parameter name='age 1-3'>От 1 до 3 лет</Parameter>
-							<Parameter name='age 4-8'>От 4 до 8 лет</Parameter>
-							<Parameter name='age 8'>Больше 8 лет</Parameter>
-						</div>
-					</details>
-					<details className='filter__item'>
-						<summary className='item__subtitle'>
-							Порода
-						</summary>
-						<div className='item__body'>
-							<Parameter name='breed ovcharka'>Овчарка</Parameter>
-							<Parameter name='breed retriver'>Ретривер</Parameter>
-							<Parameter name='breed pitbul'>Питбуль</Parameter>
-							<Parameter name='breed haski'>Хаски</Parameter>
-							<Parameter name='breed labrador'>Лабрадор</Parameter>
-							<Parameter name='breed taksa'>Такса</Parameter>
-						</div>
-					</details>
-					<details className='filter__item'>
-						<summary className='item__subtitle'>
-							Район
-						</summary>
-						<div className='item__body'>
-							<Parameter name='location CAO'>ЦАО</Parameter>
-							<Parameter name='location SAO'>САО</Parameter>
-							<Parameter name='location SVAO'>СВАО</Parameter>
-							<Parameter name='location VAO'>ВАО</Parameter>
-							<Parameter name='location YVAO'>ЮВАО</Parameter>
-							<Parameter name='location YAO'>ЮАО</Parameter>
-							<Parameter name='location YZAO'>ЮЗАО</Parameter>
-							<Parameter name='location ZAO'>ЗАО</Parameter>
-							<Parameter name='location SZAO'>СЗАО</Parameter>
-							<Parameter name='location ZelAO'>ЗелАО</Parameter>
-							<Parameter name='location TAO'>ТАО</Parameter>
-							<Parameter name='location NAO'>НАО</Parameter>
-						</div>
-					</details>
+
+					<ParametersBlock title='Пол'>
+						<Parameter name='gender female'>Девочка</Parameter>
+						<Parameter name='gender male'>Мальчик</Parameter>
+					</ParametersBlock>
+
+					<ParametersBlock title='Возраст'>
+						<Parameter name='age 1'>Меньше 1 года</Parameter>
+						<Parameter name='age 1-3'>От 1 до 3 лет</Parameter>
+						<Parameter name='age 4-8'>От 4 до 8 лет</Parameter>
+						<Parameter name='age 8'>Больше 8 лет</Parameter>
+					</ParametersBlock>
+
+					<ParametersBlock title='Порода'>
+						<Parameter name='breed ovcharka'>Овчарка</Parameter>
+						<Parameter name='breed retriver'>Ретривер</Parameter>
+						<Parameter name='breed pitbul'>Питбуль</Parameter>
+						<Parameter name='breed haski'>Хаски</Parameter>
+						<Parameter name='breed labrador'>Лабрадор</Parameter>
+						<Parameter name='breed taksa'>Такса</Parameter>
+					</ParametersBlock>
+
+					<ParametersBlock title='Район'>
+						<Parameter name='location CAO'>ЦАО</Parameter>
+						<Parameter name='location SAO'>САО</Parameter>
+						<Parameter name='location SVAO'>СВАО</Parameter>
+						<Parameter name='location VAO'>ВАО</Parameter>
+						<Parameter name='location YVAO'>ЮВАО</Parameter>
+						<Parameter name='location YAO'>ЮАО</Parameter>
+						<Parameter name='location YZAO'>ЮЗАО</Parameter>
+						<Parameter name='location ZAO'>ЗАО</Parameter>
+						<Parameter name='location SZAO'>СЗАО</Parameter>
+						<Parameter name='location ZelAO'>ЗелАО</Parameter>
+						<Parameter name='location TAO'>ТАО</Parameter>
+						<Parameter name='location NAO'>НАО</Parameter>
+					</ParametersBlock>
 				</form>
 			</div>
 		</div>
