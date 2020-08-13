8d309abe044c0e8a5fe66cbe233a9ce16f99aa9e
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-08-14 00:54:52 +0300

    Removed links from header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,12 +14,6 @@ function Header() {
 			<div className={`${styles.nav__wrapper} ${styles.flex__row}`}>
 				<div className={styles.left__header}>
 					<Logo />
-					<div className={`${styles.menu__wrapper} ${styles.flex__row}`}>
-						<Link title='Открыть' link='open'/>
-						<Link title='Закрыть' link='close'/>
-						<Link title='Узнать' link='investigate'/>
-						<Link title='Найти питомца' link='seqrch'/>
-					</div>
 				</div>
 				<div className={`${styles.profile__wrapper} ${styles.flex__row}`}>
 					<Avatar source={`${profileImg}`}/>
