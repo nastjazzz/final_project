eb20908a6d3d969d227093ecd346c2d60660dee1
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-08-14 01:08:32 +0300

    Useless coma removed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
     <BrowserRouter>
         <React.StrictMode>
             <App/>
-        </React.StrictMode>,
+        </React.StrictMode>
     </BrowserRouter>,
     document.getElementById('root')
 )
