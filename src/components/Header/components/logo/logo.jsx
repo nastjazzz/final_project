9adade409a9505db3de7bf76a1541c0a6175aba2
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-08-17 02:41:34 +0300

    Returned logo nav impovements

--- src/components/Header/components/logo/logo.jsx
+++ src/components/Header/components/logo/logo.jsx
@@ -3,7 +3,7 @@ import styles from './logo.module.css';
 
 function Logo() {
     return (
-        <div className={`${styles.logo__wrapper} ${styles.flex__row}`}>
+        <div className={`${styles.logo__wrapper} ${styles.flex__row}`} onClick={() => { window.location.href = '/'; }}>
             <div className={styles.logo__wrapper}>
                 <div id={styles.text__logo}>MasterPet</div>
             </div>
